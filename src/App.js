38cf490cada12f4e337bf4d1005e52e2a7d4a3c4
jiho kim <sokojiho2@gmail.com>
2022-06-06 13:29:35 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { HeaderTag } from "./header";
 
+function NavTag() {
+  return (
+    <nav>
+      <ol>
+        <li>
+          <a href="/read/1">html</a>
+        </li>
+        <li>
+          <a href="/read/2">css</a>
+        </li>
+        <li>
+          <a href="/read/3">JavaScript</a>
+        </li>
+      </ol>
+    </nav>
+  );
+}
+function ArticleTag() {
+  return (
+    <article>
+      <h2>Welcome</h2>
+      hello,web!
+    </article>
+  );
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HeaderTag></HeaderTag>
+      <NavTag></NavTag>
+      <ArticleTag></ArticleTag>
     </div>
   );
 }
---
+++ src/header.js
@@ -0,0 +1,11 @@
+function HeaderTag() {
+  return (
+    <header>
+      <h1>
+        <a href="/">WWWdhdh?WW</a>
+      </h1>
+    </header>
+  );
+}
+
+export { HeaderTag };
